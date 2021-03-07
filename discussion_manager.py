207d5c3727301f6bd34092fffd7f8db8474e9c53
chalasthilo <61540850+chalasthilo@users.noreply.github.com>
2021-03-07 14:50:14 +0100

    Update discussion_manager.py

--- discussion_manager.py
+++ discussion_manager.py
@@ -24,17 +24,6 @@ discussions = [{"discussionname": 'Mainaccount',
                 "lastcommands": [],
                 "newcommandsfound": False},
 
-                {"discussionname": 'Maths expertes Tle',
-                "padletlink": "https://padlet.com/julietierno/ksuy76z8oopbxk7e",
-                "categories": ["DS, DM et interros", "Progression Prevue", "Chapitre 1 Cours", "Chapitre 2 Cours", "Chapitre 3 Cours", "Chapitre 4 Cours", "Chapitre 5 Cours", "Chapitre 6 Cours", "Chapitre 7 Cours", "Chapitre 1 Exercices", "Chapitre 2 Exercices", "Chapitre 3 Exercices", "Chapitre 4 Exercices", "Chapitre 5 Exercices", "Chapitre 6 Exercices", "Chapitre 7 Exercices", "Images a trier"],
-                "catcodes": ["DSDM", "PP", "C1C", "C2C", "C3C", "C4C", "C5C", "C6C", "C7C", "C1EX", "C2EX", "C3EX", "C4EX", "C5EX", "C6EX", "C7EX", "VOID"],
-                "catids": ["section-40996450", "section-41017892", "section-40996545", "section-40996706", "section-40996607", "section-40996788", "section-40997070", "section-40996932", "section-40997157", "section-40996563", "section-40996739", "section-40996630", "section-40996816", "section-40997097", "section-40996954", "section-40997172", "section-52235332"],
-                "messages": [],
-                "lastmessages": [],
-                "newmessagefound": False,
-                "commands": [],
-                "lastcommands": [],
-                "newcommandsfound": False}
                 ]
 ###################################################################################################
 discussions_a_ajouter = [{"discussionname": 'Mainaccount',
